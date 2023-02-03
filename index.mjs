c15538c1116a973dcf2a1105ed6cbad1e49701b8
Serge Smirnov <sms-system@mail.ru>
2023-02-03 19:36:12 +0100

    Export parser func

--- index.mjs
+++ index.mjs
@@ -1,4 +1,6 @@
-import { endpoints, caches } from "./parser.mjs"
+import parse from "./parser.mjs"
+
+const { caches, endpoints } = parse('./in.txt')
 
 console.log(caches[0])
 console.log(endpoints[0].caches[0])
--- parser.mjs
+++ parser.mjs
@@ -1,71 +1,72 @@
 import { readFileSync } from 'node:fs'
 
-const lines = readFileSync('./in.txt', 'utf8').split('\n')
+export default function(filename) {
+    const lines = readFileSync(filename, 'utf8').split('\n')
 
-
-const endpoints = []
-const requestDescriptions = []
-const caches = []
-
-let cursor = 0
-
-const [videosCount, endpointsCount, requestDescriptionsCount, cachesCount, cachesCapacity] = lines[cursor++].split(' ').map(x => Number(x))
-
-for (let i = 0; i < cachesCount; i++) {
-    caches.push({
-        cachesCapacity,
-    })
-}
-
-const videos = lines[cursor++].split(' ').map(x => ({
-    size: Number(x)
-}))
-
-for (let i = 0; i < endpointsCount; i++) {
-    const [latency, cachesCount] = lines[cursor++].split(' ')
-    const endpoint = {
-        latency: Number(latency),
-        caches: []
-    }
-    for (let j = 0; j < Number(cachesCount); j++) {
-        const [ cacheID, latency ] = lines[cursor++].split(' ')
-        endpoint.caches.push({
-            cache: {
-                id: Number(cacheID),
-                data: caches[Number(cacheID)]
-            },
-            latency: Number(latency)
+    const endpoints = []
+    const requestDescriptions = []
+    const caches = []
+    
+    let cursor = 0
+    
+    const [videosCount, endpointsCount, requestDescriptionsCount, cachesCount, cachesCapacity] = lines[cursor++].split(' ').map(x => Number(x))
+    
+    for (let i = 0; i < cachesCount; i++) {
+        caches.push({
+            cachesCapacity,
         })
     }
-    endpoints.push(endpoint)
-}
-
-for (let i = 0; i < requestDescriptionsCount; i++) {
-    const [videoId, endpointID, count] = lines[cursor++].split(' ')
-    const requestDescription = {
-        count: Number(count),
-        video: {
-            id: Number(videoId),
-            data: videos[Number(videoId)]
-        },
-        endpoint: {
-            id: Number(endpointID),
-            data: endpoints[Number(endpointID)]
+    
+    const videos = lines[cursor++].split(' ').map(x => ({
+        size: Number(x)
+    }))
+    
+    for (let i = 0; i < endpointsCount; i++) {
+        const [latency, cachesCount] = lines[cursor++].split(' ')
+        const endpoint = {
+            latency: Number(latency),
+            caches: []
         }
+        for (let j = 0; j < Number(cachesCount); j++) {
+            const [ cacheID, latency ] = lines[cursor++].split(' ')
+            endpoint.caches.push({
+                cache: {
+                    id: Number(cacheID),
+                    data: caches[Number(cacheID)]
+                },
+                latency: Number(latency)
+            })
+        }
+        endpoints.push(endpoint)
     }
-    requestDescriptions.push(requestDescription)
-}
+    
+    for (let i = 0; i < requestDescriptionsCount; i++) {
+        const [videoId, endpointID, count] = lines[cursor++].split(' ')
+        const requestDescription = {
+            count: Number(count),
+            video: {
+                id: Number(videoId),
+                data: videos[Number(videoId)]
+            },
+            endpoint: {
+                id: Number(endpointID),
+                data: endpoints[Number(endpointID)]
+            }
+        }
+        requestDescriptions.push(requestDescription)
+    }
 
-export {
-    cachesCapacity,
+    return {
+        cachesCapacity,
 
-    videosCount,
-    endpointsCount,
-    requestDescriptionsCount,
-    cachesCount,
-
-    endpoints,
-    requestDescriptions,
-    caches,
-    videos
+        videosCount,
+        endpointsCount,
+        requestDescriptionsCount,
+        cachesCount,
+    
+        endpoints,
+        requestDescriptions,
+        caches,
+        videos
+    }
 }
